f70ad819748200bf48cb13473ae47919fe6056ae
Niels Vinke <nielsvinke@live.nl>
2020-12-23 20:08:16 +0100

    reset mediaObj on previewImage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,40 +13,6 @@ function resetMediaObj(mediaObj: MediaObj){
   mediaObj.mediaType = '';
 }
 
-export function getSecureMediaRedditVideo(post: Post): MediaObj {
-  resetMediaObj(mediaObj);
-  if (_.isObject(post.secure_media) && post.secure_media.reddit_video) {
-    mediaObj.subredditName = post.subreddit;
-    mediaObj.imageSource = post.secure_media.reddit_video.fallback_url;
-    mediaObj.mediaType = post.secure_media.reddit_video.is_gif ? 'gif' : 'video';
-  }
-
-  return mediaObj;
-}
-
-export function getMediaRedditVideo(post: Post): MediaObj {
-  resetMediaObj(mediaObj);
-  if (_.isObject(post.media) && post.media.reddit_video) {
-    mediaObj.subredditName = post.subreddit;
-    mediaObj.imageSource = post.media.reddit_video.fallback_url;
-    mediaObj.mediaType = post.media.reddit_video.is_gif ? 'gif' : 'video';
-  }
-
-  return mediaObj;
-}
-
-export function getSecureMediaEmbedVideo(post: Post): MediaObj {
-  resetMediaObj(mediaObj);
-  if (_.isObject(post.secure_media_embed) && !_.isEmpty(post.secure_media_embed)) {
-    const secureMediaEmbedPost = post.secure_media_embed;
-    mediaObj.subredditName = post.subreddit;
-    mediaObj.imageSource = secureMediaEmbedPost.media_domain_url;
-    mediaObj.mediaType = 'video';
-  }
-
-  return mediaObj;
-}
-
 export function getPreviewImage(post: Post): MediaObj {
   resetMediaObj(mediaObj);
   if (_.isObject(post.preview)) {
@@ -60,12 +26,42 @@ export function getPreviewImage(post: Post): MediaObj {
 }
 
 export function getPreviewGif(post: Post): MediaObj {
-  resetMediaObj(mediaObj);
   if (_.isObject(post.preview) && !_.isEmpty(post.preview.reddit_video_preview)) {
     const gifPreviewPost = post.preview.reddit_video_preview;
     mediaObj.subredditName = post.subreddit;
     mediaObj.imageSource = gifPreviewPost.fallback_url;
     mediaObj.mediaType = 'gif';
+  }
+
+  return mediaObj;
+}
+
+export function getSecureMediaRedditVideo(post: Post): MediaObj {
+  if (_.isObject(post.secure_media) && post.secure_media.reddit_video) {
+    mediaObj.subredditName = post.subreddit;
+    mediaObj.imageSource = post.secure_media.reddit_video.fallback_url;
+    mediaObj.mediaType = post.secure_media.reddit_video.is_gif ? 'gif' : 'video';
+  }
+
+  return mediaObj;
+}
+
+export function getMediaRedditVideo(post: Post): MediaObj {
+  if (_.isObject(post.media) && post.media.reddit_video) {
+    mediaObj.subredditName = post.subreddit;
+    mediaObj.imageSource = post.media.reddit_video.fallback_url;
+    mediaObj.mediaType = post.media.reddit_video.is_gif ? 'gif' : 'video';
+  }
+
+  return mediaObj;
+}
+
+export function getSecureMediaEmbedVideo(post: Post): MediaObj {
+  if (_.isObject(post.secure_media_embed) && !_.isEmpty(post.secure_media_embed)) {
+    const secureMediaEmbedPost = post.secure_media_embed;
+    mediaObj.subredditName = post.subreddit;
+    mediaObj.imageSource = secureMediaEmbedPost.media_domain_url;
+    mediaObj.mediaType = 'video';
   }
 
   return mediaObj;
